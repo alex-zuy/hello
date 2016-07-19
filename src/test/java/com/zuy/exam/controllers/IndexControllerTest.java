0848fa2ef0e68a7adbfcffdfc1b1b15f11a05e71
Zuy Alexey <zuy.alexey@gmail.com>
2016-07-19 19:35:20 +0300

    Added IndexController tests

---
+++ src/test/java/com/zuy/exam/controllers/IndexControllerTest.java
@@ -0,0 +1,65 @@
+package com.zuy.exam.controllers;
+
+import com.zuy.exam.App;
+import com.zuy.exam.config.TestDatabaseConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {App.class, TestDatabaseConfig.class})
+@Sql({"/schema.sql", "/insert_admin.sql"})
+public class IndexControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+            .apply(springSecurity())
+            .build();
+    }
+
+    @Test
+    public void testUnauthenticatedUserIndexPage() throws Exception {
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("login"));
+    }
+    
+    @Test
+    public void testAuthenticatedUserIndexPage() throws Exception {
+        mockMvc.perform(get("/").with(user("admin")))
+            .andExpect(status().isOk())
+            .andExpect(view().name("greeting"));
+    }
+
+    @Test
+    public void testFormLogin() throws Exception {
+        mockMvc.perform(
+            formLogin("/")
+                .user("admin")
+                .password("password"))
+            .andExpect(authenticated().withUsername("admin"));
+    }
+}
